6b8c0bcd22098677fbeb93978e1a5471433b644a
Burganova Renata <st117269@student.spbu.ru>
2023-12-07 17:26:36 +0300

    Create main menu

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 
 int main() {
     std::system("chcp 65001");
-    std::cout << "Привет!" << std::endl;
+
+    int user_input;
+    do {
+        std::cout << "1 - Хочу учиться!" << std::endl;
+        std::cout << "0 - Выйти отсюда" << std::endl;
+        std::cout << "Выберите действие > ";
+        std::cin >> user_input;
+    } while (user_input != 0);
+
     return 0;
 }
